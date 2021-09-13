70e5bc1a66356a0deb878c233f2d46d2cdea52ab
chiochiwo <ccu12345@naver.com>
2021-09-13 14:44:18 +0900

    6교시 커밋

--- test7/test.py
+++ test7/test.py
@@ -25,3 +25,10 @@ for i in range(1,11):
 
 
 #리스트 활용
+l1=[1,100,25,47,-12,5,-1234]
+for i in l1:
+    print(i)
+
+
+for i in range(len(l1)):
+    print(l1[i])  
---
+++ test7/test1.py
@@ -0,0 +1,6 @@
+l1=[1,100,25,47,-12,5,-1234]
+for i in l1:
+    print(i)
+
+for i in range(len(l1)):
+    print(l1[i])   
